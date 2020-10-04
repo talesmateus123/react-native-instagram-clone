ed9d26c27e46a4f722bf4ed1ddfcd0867b59c3c1
Tales Ferreira <talesmateus1999@hotmail.com>
2020-10-04 15:07:28 -0400

    Implemented Post component and your dependents (Comments and Author)

--- App.js
+++ App.js
@@ -9,18 +9,37 @@
 import React, { Component } from 'react';
 import {
   SafeAreaView,
+  View,
   Text,
   StyleSheet
 } from 'react-native';
 
 import Header from './src/components/Header'
+import Post from './src/components/Post'
 
 export default class App extends Component {
   render() {
+
+    const comments = [
+      {
+        nickname: 'Joana Elena Silva',
+        comment: 'Excelente foto'
+      },
+      {
+        nickname: 'Rafael Gustavo Pereira',
+        comment: 'Muito ruim! Faço melhor...'
+      }
+    ]
+
     return (
       <>
-        <SafeAreaView>
-          <Header></Header>
+        <SafeAreaView style={{flex: 1}}>
+          <Header />
+          <Post 
+            image={require('./assets/imgs/fence.jpg')}
+            comments={comments}
+          />
+
         </SafeAreaView>
       </>
     );
---
+++ src/components/Author.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { 
+    StyleSheet, View, Image, 
+    Text, Dimensions 
+} from 'react-native'
+import { Gravatar } from 'react-native-gravatar'
+
+export default props => {
+    return (
+        <View style={styles.container}>
+            <Gravatar 
+                options={{
+                    email: props.email,
+                    secure: true
+                }}
+                style={styles.avatar} />
+            <Text style={styles.nickname}>{props.nickname}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        marginHorizontal: 10
+    },
+    nickname: {
+        color: '#444',
+        marginVertical: 10,
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+})
