fb0bd8b207f7df1e80a60234faf90b9e5a8d591e
Tales Ferreira <talesmateus1999@hotmail.com>
2020-10-06 23:31:36 -0400

    Implemented AddPhoto module

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
+import AddPhoto from './screens/AddPhoto'
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
 const Tabs = () => (
     <Tab.Navigator
         initialRouteName="Feed"
         tabBarOptions={{
             activeTintColor: '#e91e63',
+            showLabel: false
         }}
     >
         <Tab.Screen
@@ -20,12 +22,13 @@ const Tabs = () => (
             component={Feed}
             options={{
                 tabBarLabel: 'Home',
+
                 tabBarIcon: ({ tintColor }) => <Icon name="home" size={30} color={ tintColor } />
             }}
         />
         <Tab.Screen
             name="AddPhoto"
-            component={Feed}
+            component={AddPhoto}
             options={{
                 tabBarLabel: 'Updates',
                 tabBarIcon: ({ tintColor }) => <Icon name="camera" size={30} color={ tintColor } />
--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {
   View, Text, StyleSheet,
-  TextInput,TouchableWithoutFeedback as TWF,
+  TextInput, TouchableWithoutFeedback as TWF,
   Alert
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'
